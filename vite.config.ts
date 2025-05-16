fa11e319b9dc3e30ef55058cbf3cd5c534c8209e
Mohammed Fakih <mhd.fakih@gmail.com>
2025-05-16 14:51:17 +0300

    update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
@@ -21,6 +21,7 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      base: "/",
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
