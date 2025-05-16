9fee201af87d1c3783dbf06ef0da3211d15b7236
Mohammed Fakih <mhd.fakih@gmail.com>
2025-05-16 15:11:04 +0300

    build update

--- dist/index.js
+++ dist/index.js
@@ -185,7 +185,8 @@ var vite_config_default = defineConfig({
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets")
+      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      base: "/"
     }
   },
   root: path.resolve(import.meta.dirname, "client"),
